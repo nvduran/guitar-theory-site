974b2b4bf6175e7bc202cca19c415f3e695a9cbb
nvduran <nvduran@gmail.com>
2022-09-17 17:03:47 -0500

    bootstrap and scss working

--- src/components/FretboardView/index.js
+++ src/components/FretboardView/index.js
@@ -1,5 +1,11 @@
 import React from "react";
+import "../../styles/FretboardView.css";
+import { Container } from "react-bootstrap";
 
 export default function FretboardView() {
-  return <div></div>;
+  return (
+    <div>
+      <Container className="fb-container">123</Container>
+    </div>
+  );
 }
