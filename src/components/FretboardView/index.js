b25f62d0f3e45784a2814d4a5b5ac4ad2d65d02b
nvduran <nvduran@gmail.com>
2022-09-25 13:59:00 -0500

    basic fretboard

--- src/components/FretboardView/index.js
+++ src/components/FretboardView/index.js
@@ -5,7 +5,56 @@ import { Container } from "react-bootstrap";
 export default function FretboardView() {
   return (
     <div>
-      <Container className="fb-container">123</Container>
+      <div className="fretboard-container">
+        <div className="fret fret1">
+          <div className="string"></div>
+          <div className="string"></div>
+          <div className="string"></div>
+          <div className="string"></div>
+          <div className="string"></div>
+          <div className="string"></div>
+        </div>
+        <div className="fret fret2">
+          <div className="string"></div>
+          <div className="string"></div>
+          <div className="string"></div>
+          <div className="string"></div>
+          <div className="string"></div>
+          <div className="string"></div>
+        </div>
+        <div className="fret fret3">
+          <div className="string"></div>
+          <div className="string"></div>
+          <div className="string"></div>
+          <div className="string"></div>
+          <div className="string"></div>
+          <div className="string"></div>
+        </div>
+        <div className="fret fret4">
+          <div className="string"></div>
+          <div className="string"></div>
+          <div className="string"></div>
+          <div className="string"></div>
+          <div className="string"></div>
+          <div className="string"></div>
+        </div>
+        <div className="fret fret5">
+          <div className="string"></div>
+          <div className="string"></div>
+          <div className="string"></div>
+          <div className="string"></div>
+          <div className="string"></div>
+          <div className="string"></div>
+        </div>
+        <div className="fret fret6">
+          <div className="string"></div>
+          <div className="string"></div>
+          <div className="string"></div>
+          <div className="string"></div>
+          <div className="string"></div>
+          <div className="string"></div>
+        </div>
+      </div>
     </div>
   );
 }
