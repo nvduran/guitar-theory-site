19c828a232a76e17e17c0069a530a675118b5f9a
nvduran <nvduran@gmail.com>
2022-11-27 20:18:30 -0600

    basic navbar added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import PentatonicPage from "./components/PentatonicPage";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <PentatonicPage />
       <div></div>
     </div>
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import "../../styles/NavBar.css";
+
+export default function NavBar() {
+  return (
+    <div>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        variant="light"
+        className="navbar-main-div"
+      >
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
