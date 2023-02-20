8b9ae6c13ddce55649b55cda130447fdb85622fb
nvduran <nvduran@gmail.com>
2023-02-19 20:39:58 -0600

    nav styling

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -7,27 +7,25 @@ import SayWhat from "../SayWhat";
 import "../../styles/NavBar.css";
 
 export default function NavBar() {
-  return (
-    <div>
-      <Navbar
-        collapseOnSelect
-        expand="lg"
-        variant="light"
-        className="navbar-main-div"
-      >
-        <Container className="nav-bs-container">
-          <Navbar.Brand href="/">Guitar Theory</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/">Charts</Nav.Link>
-              <Nav.Link href="/AboutPentatonics">About Pentatonics</Nav.Link>
-              <Nav.Link href="/PrivacyPolicy">Privacy Policy</Nav.Link>
-              <Nav.Link href="/About">About</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
-  );
+        return (
+                <div>
+                        <Navbar collapseOnSelect expand="lg" variant="light" className="navbar-main-div">
+                                <Container className="nav-bs-container">
+                                        <Navbar.Brand href="/">
+                                                <div className="nav-guitar-div">Guitar-</div>
+                                                <div className="nav-theory-div">Theory</div>
+                                        </Navbar.Brand>
+                                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                                        <Navbar.Collapse id="responsive-navbar-nav">
+                                                <Nav className="me-auto">
+                                                        <Nav.Link href="/">Charts</Nav.Link>
+                                                        <Nav.Link href="/AboutPentatonics">About Pentatonics</Nav.Link>
+                                                        <Nav.Link href="/PrivacyPolicy">Privacy Policy</Nav.Link>
+                                                        <Nav.Link href="/About">About</Nav.Link>
+                                                </Nav>
+                                        </Navbar.Collapse>
+                                </Container>
+                        </Navbar>
+                </div>
+        );
 }
