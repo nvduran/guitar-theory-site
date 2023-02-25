184dc204874765403adf3291886e7586ea0bf166
nvduran <nvduran@gmail.com>
2023-02-25 16:51:21 -0600

    all notes

--- src/components/FullFretboard/index.js
+++ src/components/FullFretboard/index.js
@@ -466,6 +466,16 @@ export default function FullFretboard() {
                                 <select value={userFilters.keyRoot} onChange={handleRootChange} className="">
                                         <option value="C">C</option>
                                         <option value="C#">C#</option>
+                                        <option value="D">D</option>
+                                        <option value="D#">D#</option>
+                                        <option value="E">E</option>
+                                        <option value="F">F</option>
+                                        <option value="F#">F#</option>
+                                        <option value="G">G</option>
+                                        <option value="G#">G#</option>
+                                        <option value="A">A</option>
+                                        <option value="A#">A#</option>
+                                        <option value="B">B</option>
                                 </select>
                         </div>
                 </div>
