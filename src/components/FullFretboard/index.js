5b2bde2e1b59f0d3d98d72286c4688b04cbd1ae1
nvduran <nvduran@gmail.com>
2023-02-25 11:44:24 -0600

    fret outlines with side scrolling

--- src/components/FullFretboard/index.js
+++ src/components/FullFretboard/index.js
@@ -1,5 +1,35 @@
 import React from "react";
+import "../../styles/FullFretboard.css";
 
 export default function FullFretboard() {
-        return <div>index</div>;
+        return (
+                <div className="big-full-fretboard-container">
+                        <div className="big-full-fretboard-div">
+                                <div className="ff-fret1"></div>
+                                <div className="ff-fret2"></div>
+                                <div className="ff-fret3"></div>
+                                <div className="ff-fret4"></div>
+                                <div className="ff-fret5"></div>
+                                <div className="ff-fret6"></div>
+                                <div className="ff-fret7"></div>
+                                <div className="ff-fret8"></div>
+                                <div className="ff-fret9"></div>
+                                <div className="ff-fret10"></div>
+                                <div className="ff-fret11"></div>
+                                <div className="ff-fret12"></div>
+                                <div className="ff-fret13"></div>
+                                <div className="ff-fret14"></div>
+                                <div className="ff-fret15"></div>
+                                <div className="ff-fret16"></div>
+                                <div className="ff-fret17"></div>
+                                <div className="ff-fret18"></div>
+                                <div className="ff-fret19"></div>
+                                <div className="ff-fret20"></div>
+                                <div className="ff-fret21"></div>
+                                <div className="ff-fret22"></div>
+                                <div className="ff-fret23"></div>
+                                <div className="ff-fret24"></div>
+                        </div>
+                </div>
+        );
 }
